02b7c127162a5986309abef23bd44563c90c8d0c
Marwa <marwalabidi255@gmail.com>
2025-04-04 18:23:16 +0100

    add Fields components

---
+++ Frontend/src/components/Fields/SearchBar.jsx
@@ -0,0 +1,16 @@
+import { Form, Row, Col, Button } from "react-bootstrap";
+
+const SearchBar = ({ searchTerm, setSearchTerm }) => (
+  <Form.Group as={Row} className="mb-0 me-3">
+    <Col xs="auto">
+      <Form.Control
+        type="text"
+        placeholder="Search students..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </Col>
+  </Form.Group>
+);
+
+export default SearchBar;
