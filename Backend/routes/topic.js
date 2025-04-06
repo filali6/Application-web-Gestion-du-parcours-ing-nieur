3835458afee5b33fd8fa39f8b5796540b635a26a
chaima filali <chaimafilali6@gmail.com>
2025-04-06 16:26:24 +0100

    student s

--- Backend/routes/topic.js
+++ Backend/routes/topic.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { addTopic } from "../controllers/topic.js";
+import { addTopic,getTopics } from "../controllers/topic.js";
 import { loggedMiddleware } from "../middlewares/authMiddlewares.js";
 import { isStudent } from "../middlewares/roleMiddlewares.js";
 import { topicUpload } from "../middlewares/topicUpload.js"; // Import du middleware global
@@ -14,6 +14,13 @@ router.post(
   isStudent,
   topicUpload.array("documents", 5), // Jusqu'à 5 fichiers avec contrôle des types
   addTopic
+);
+router.get(
+  "/topics/drop",
+  loggedMiddleware,
+  isStudent,
+
+  getTopics
 );
 
 export default router;
